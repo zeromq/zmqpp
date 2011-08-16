528858370f2776c557d947991c63d3105b5fe1c9
Ben Gray <@benjamg>
2011-08-16 18:07:25 +0100

    added a basic client application

---
+++ src/client/main.cpp
@@ -0,0 +1,201 @@
+/*
+ *  Created on: 16 Aug 2011
+ *      Author: @benjamg
+ */
+
+#include <array>
+#include <iostream>
+#include <string>
+#include <tuple>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include <zmq/zmq.hpp>
+
+boost::program_options::options_description connection_options()
+{
+	boost::program_options::options_description options("Connection Options");
+	options.add_options()
+		("bind", "bind to specified endpoint instead of connecting")
+		;
+
+	return options;
+}
+
+boost::program_options::options_description miscellaneous_options()
+{
+	boost::program_options::options_description options("Miscellaneous Options");
+	options.add_options()
+		("version", "display version")
+		("help", "show this help page")
+		;
+
+	return options;
+}
+
+void usage(std::string const& executable_name)
+{
+	std::cout << "Usage: " << executable_name << " [options] SOCKETTYPE ENDPOINT" << std::endl;
+	std::cout << "0mq command line client tool." << std::endl;
+	std::cout << "SOCKETTYPE is one of the supported 0mq socket types." << std::endl;
+	std::cout << "ENDPOINT is any valid 0mq endpoint." << std::endl;
+}
+
+typedef std::tuple<zmq::socket_type, bool, bool> socket_type_data;
+
+int main(int argc, char const* argv[])
+{
+	boost::program_options::positional_options_description arguments;
+	arguments.add("type", 1);
+	arguments.add("endpoint", 1);
+
+	boost::program_options::options_description all;
+	all.add_options()
+		("type", "0mq socket type")
+		("endpoint", "0mq endpoint")
+		;
+	all.add(miscellaneous_options());
+	all.add(connection_options());
+
+	boost::program_options::variables_map vm;
+	bool usage = false;
+
+	std::map<std::string, socket_type_data> socket_types;
+	socket_types["push"] = socket_type_data(zmq::socket_type::push, true, false);
+	socket_types["pull"] = socket_type_data(zmq::socket_type::pull, false, true);
+
+	socket_types["pub"] = socket_type_data(zmq::socket_type::publish, true, false);
+	socket_types["publish"] = socket_type_data(zmq::socket_type::publish, true, false);
+	socket_types["sub"] = socket_type_data(zmq::socket_type::subcribe, false, true);
+	socket_types["subscribe"] = socket_type_data(zmq::socket_type::subcribe, false, true);
+
+	try {
+		boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(all).positional(arguments).run(), vm);
+	}
+	catch(boost::program_options::too_many_positional_options_error& e)
+	{
+		std::cout << "Too many arguments provided." << std::endl;
+		usage = true;
+	}
+	catch(boost::program_options::unknown_option& e)
+	{
+		std::cout << "Unknown option '" << e.get_option_name() << "'." << std::endl;
+		usage = true;
+	}
+
+	if (vm.count("type") && (socket_types.end() == socket_types.find(vm["type"].as<std::string>())))
+	{
+		std::cout << "Unknown value '" << vm["type"].as<std::string>() << "' provided for 0mq socket type." << std::endl;
+		usage = true;
+	}
+
+	if (vm.count("version"))
+	{
+		std::cout << (strrchr(argv[0], '/') + 1) << " version " << BUILD_VERSION_API << "." << BUILD_VERSION_REVISION << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	if (usage || 0 == vm.count("type") || 0 == vm.count("endpoint") || vm.count("help"))
+	{
+		std::cout << "Usage: " << (strrchr(argv[0], '/') + 1) << " [options] SOCKETTYPE ENDPOINT" << std::endl;
+		std::cout << "0mq command line client tool." << std::endl;
+		std::cout << "SOCKETTYPE is one of the supported 0mq socket types." << std::endl;
+
+		auto it = socket_types.begin();
+		std::cout << "  " << (*it).first;
+		for(++it; it != socket_types.end(); ++it)
+		{
+			std::cout << ", " << (*it).first;
+		}
+		std::cout << std::endl;
+
+		std::cout << "ENDPOINT is any valid 0mq endpoint." << std::endl;
+		std::cout << std::endl;
+
+		if (!vm.count("help"))
+		{
+			return EXIT_FAILURE;
+		}
+
+		std::cout << connection_options() << std::endl;
+		std::cout << miscellaneous_options() << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	int standardin = fileno(stdin);
+	if (standardin < 0) // really?
+	{
+		std::cout << "Unable to get standard input, this might be an OS thing, sorry." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	socket_type_data data = socket_types[vm["type"].as<std::string>()];
+	zmq::socket_type type = std::get<0>(data);
+	bool can_send = std::get<1>(data);
+	bool can_recv = std::get<2>(data);
+	std::string endpoint = vm["endpoint"].as<std::string>();
+
+	zmq::context context;
+	zmq::socket socket(context, type);
+	if (vm.count("bind"))
+	{
+		socket.bind(endpoint);
+	}
+	else
+	{
+		socket.connect(endpoint);
+	}
+
+	zmq::poller poller;
+	poller.add(socket);
+	poller.add(standardin);
+
+	if (can_send)
+	{
+		std::cout << "While sending packets is allowed data entered on standard in will be sent to the 0mq socket." << std::endl;
+		std::cout << "messages will be considered terminated by newline." << std::endl;
+		std::cout << std::endl;
+	}
+
+	while(true)
+	{
+		poller.check_for(socket, (can_recv) ? zmq::poller::POLL_IN : zmq::poller::POLL_NONE);
+		poller.check_for(standardin, (can_send) ? zmq::poller::POLL_IN : zmq::poller::POLL_NONE);
+
+		if(poller.poll())
+		{
+			if (poller.has_input(socket))
+			{
+				assert(can_recv);
+
+				std::string message;
+				socket.receive(message);
+				std::cout << "inbound: " << message << std::endl;
+			}
+
+			if (poller.has_input(standardin))
+			{
+				assert(can_send);
+
+				std::array<char, 512> buffer;
+				char* result = fgets(buffer.data(), buffer.size(), stdin);
+				if (nullptr == result)
+				{
+					std::cout << "error in standard input" << std::endl;
+					return EXIT_FAILURE;
+				}
+
+				//buffer.data[strlen(buffer.data()) - 1] = 0;
+				std::cout << "outbound: " << buffer.data() << std::endl;
+				if (!socket.send_raw(buffer.data(), strlen(buffer.data()), zmq::socket::DONT_WAIT))
+				{
+					std::cout << "send failed, socket would have blocked" << std::endl;
+				}
+			}
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ src/zmq/poller.cpp
@@ -0,0 +1,98 @@
+/*
+ *  Created on: 16 Aug 2011
+ *      Author: Ben Gray (@benjamg)
+ */
+
+#include "socket.hpp"
+#include "poller.hpp"
+
+namespace zmq
+{
+
+const long poller::WAIT_FOREVER;
+const int poller::POLL_NONE;
+const int poller::POLL_IN;
+const int poller::POLL_OUT;
+const int poller::POLL_ERROR;
+
+poller::poller()
+	: _items()
+	, _index()
+{
+
+}
+
+void poller::add(socket& socket, short const& event /* = POLL_IN */)
+{
+	zmq_pollitem_t item { socket, 0, event, 0 };
+
+	size_t index = _items.size();
+	_items.push_back(item);
+	_index[socket] = index;
+}
+
+void poller::add(int const& descriptor, short const& event /* = POLL_IN */)
+{
+	zmq_pollitem_t item { nullptr, descriptor, event, 0 };
+
+	size_t index = _items.size();
+	_items.push_back(item);
+	_fdindex[descriptor] = index;
+}
+
+void poller::check_for(socket const& socket, short const& event)
+{
+	auto found = _index.find(socket);
+	if (_index.end() == found)
+	{
+		throw exception("this socket is not represented within this poller");
+	}
+
+	_items[(*found).second].events = event;
+}
+
+void poller::check_for(int const& descriptor, short const& event)
+{
+	auto found = _fdindex.find(descriptor);
+	if (_fdindex.end() == found)
+	{
+		throw exception("this socket is not represented within this poller");
+	}
+
+	_items[(*found).second].events = event;
+}
+
+bool poller::poll(long timeout /* = WAIT_FOREVER */)
+{
+	int result = zmq_poll(_items.data(), _items.size(), timeout);
+	if (result < 0)
+	{
+		throw zmq_internal_exception();
+	}
+
+	return (result > 0);
+}
+
+short poller::events(socket const& socket)
+{
+	auto found = _index.find(socket);
+	if (_index.end() == found)
+	{
+		throw exception("this socket is not represented within this poller");
+	}
+
+	return _items[(*found).second].revents;
+}
+
+short poller::events(int const& descriptor)
+{
+	auto found = _fdindex.find(descriptor);
+	if (_fdindex.end() == found)
+	{
+		throw exception("this file descriptor is not represented within this poller");
+	}
+
+	return _items[(*found).second].revents;
+}
+
+}
