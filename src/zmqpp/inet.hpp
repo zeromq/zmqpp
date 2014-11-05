15fd4c6cd22f474266ede4e9d92b65f5e7321a04
Arnaud Kapp <kapp.arno@gmail.com>
2014-11-05 22:19:42 +0100

    Alternative yosemite fix from zeromq upstream commit 9ba540ce31f54f1566a70c7f5aa71f5c22634836

--- src/zmqpp/inet.hpp
+++ src/zmqpp/inet.hpp
@@ -72,27 +72,33 @@ inline uint64_t swap_if_needed(uint64_t const value_to_check)
  * 64 bit version of the htons/htonl
  *
  * I've used the name htonll to try and keep with the htonl naming scheme.
+ * We do not define this function if the macro `htonll` exists.
  *
  * \param hostlonglong unsigned 64 bit host order integer
  * \return unsigned 64 bit network order integer
  */
+#ifndef htonll
 inline uint64_t htonll(uint64_t const hostlonglong)
 {
 	return zmqpp::swap_if_needed(hostlonglong);
 }
+#endif
 
 /*!
  * 64 bit version of the ntohs/ntohl
  *
  * I've used the name htonll to try and keep with the htonl naming scheme.
+ * We do not define this function if the macro `ntohll` exists.
  *
  * \param networklonglong unsigned 64 bit network order integer
  * \return unsigned 64 bit host order integer
  */
+#ifndef ntohll
 inline uint64_t ntohll(uint64_t const networklonglong)
 {
 	return zmqpp::swap_if_needed(networklonglong);
 }
+#endif
 
 /*!
  * floating point version of the htons/htonl
@@ -142,7 +148,7 @@ inline double htond(double value)
 
 	uint64_t temp;
 	memcpy(&temp, &value, sizeof(uint64_t));
-	temp = zmqpp::htonll(temp);
+	temp = htonll(temp);
 	memcpy(&value, &temp, sizeof(uint64_t));
 
 	return value;
@@ -160,7 +166,7 @@ inline double ntohd(double value)
 
 	uint64_t temp;
 	memcpy(&temp, &value, sizeof(uint64_t));
-	temp = zmqpp::ntohll(temp);
+	temp = ntohll(temp);
 	memcpy(&value, &temp, sizeof(uint64_t));
 
 	return value;
