f0d7d030d91eb29e3207ddc6d7c1356c9d589f5f
Luiz Siqueira <indiosmo@gmail.com>
2017-04-27 18:30:02 -0300

    Add wrapper for zmq_proxy_steerable

---
+++ src/zmqpp/proxy_steerable.cpp
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file is part of zmqpp.
+ * Copyright (c) 2011-2015 Contributors as noted in the AUTHORS file.
+ */
+
+#include "proxy_steerable.hpp"
+
+zmqpp::proxy_steerable::proxy_steerable(
+    zmqpp::socket &sa,
+    zmqpp::socket &sb,
+    zmqpp::socket &control )
+{
+  zmq_proxy_steerable(static_cast<void *>(sa), static_cast<void *>(sb), nullptr,
+      static_cast<void *>(control));
+}
+
+zmqpp::proxy_steerable::proxy_steerable(
+    zmqpp::socket &sa,
+    zmqpp::socket &sb,
+    zmqpp::socket &control,
+    zmqpp::capture &capture )
+{
+  zmq_proxy_steerable(static_cast<void *>(sa), static_cast<void *>(sb),
+      static_cast<void *>(capture), static_cast<void *>(control));
+}
+
